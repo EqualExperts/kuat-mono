1c0e8dd9f915cc7fcbebe39ac6dee8a1f643f65c
efordee24 <ed.ford@equalexperts.com>
2025-12-16 16:35:03 +0000

    added an accordion

--- packages/kuat-react/src/index.ts
+++ packages/kuat-react/src/index.ts
@@ -7,4 +7,10 @@ import "./styles.css";
 // Components
 export { Button, buttonVariants } from "./components/ui/button";
 export type { ButtonProps } from "./components/ui/button";
+export {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "./components/ui/accordion";
 
--- packages/kuat-vue/src/index.ts
+++ packages/kuat-vue/src/index.ts
@@ -7,4 +7,10 @@ import "./styles.css";
 // Components
 export { Button, buttonVariants } from "./components/ui/button";
 export type { ButtonVariants } from "./components/ui/button";
+export {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "./components/ui/accordion";
 
