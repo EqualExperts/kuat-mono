f873f2a1dd5ca8c0b774d99b5a989deb6b0c90ba
efordee24 <ed.ford@equalexperts.com>
2026-03-03 14:47:32 +0000

    added testing framework

--- packages/kuat-react/src/test/setup.ts
+++ packages/kuat-react/src/test/setup.ts
@@ -1,1 +1,9 @@
-import "@testing-library/jest-dom";
+import { expect, afterEach } from "vitest";
+import * as matchers from "@testing-library/jest-dom/matchers";
+import { cleanup } from "@testing-library/react";
+
+expect.extend(matchers);
+
+afterEach(() => {
+  cleanup();
+});
--- packages/kuat-vue/src/test/setup.ts
+++ packages/kuat-vue/src/test/setup.ts
@@ -1,1 +1,9 @@
-import "@testing-library/jest-dom";
+import { expect, afterEach } from "vitest";
+import * as matchers from "@testing-library/jest-dom/matchers";
+import { cleanup } from "@testing-library/vue";
+
+expect.extend(matchers);
+
+afterEach(() => {
+  cleanup();
+});
